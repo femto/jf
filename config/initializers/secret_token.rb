f6becb8cd19cdbc5d9be7f80b95bb2b50934d976
femto <femtowin@gmail.com>
2012-01-22 02:19:34 +0800

    a

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jf::Application.config.secret_token = '02fb620622b476cb384368f0b622ab3d2f855485f84068145ff9d9020de28e4dcaa455772f737774e4668087952d1d0f5774aee4b159017fe414fb270b38f68e'
