2acd6fd81839fba415531d0ea12078ca4745dcf4
femto <femtowin@gmail.com>
2012-01-24 05:22:27 +0800

    renameZhNamesToZhCnNamesInProduct

---
+++ db/migrate/20120124010116_rename_zh_names_to_zh_cn_names_in_product.rb
@@ -0,0 +1,12 @@
+class RenameZhNamesToZhCnNamesInProduct < ActiveRecord::Migration
+
+  change_table :products do |t|
+    t.rename :zh_name, :zh_cn_name
+  end
+
+  change_table :categories do |t|
+    t.rename :zh_name, :zh_cn_name
+  end
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120124010115) do
+ActiveRecord::Schema.define(:version => 20120124010116) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(:version => 20120124010115) do
 
   create_table "categories", :force => true do |t|
     t.string   "en_name"
-    t.string   "zh_name"
+    t.string   "zh_cn_name"
     t.integer  "parent_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(:version => 20120124010115) do
   create_table "products", :force => true do |t|
     t.string   "en_name"
     t.text     "en_body"
-    t.string   "zh_name"
+    t.string   "zh_cn_name"
     t.text     "zh_body"
     t.text     "whitepaper"
     t.datetime "created_at"
