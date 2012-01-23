58c1e63c441a1237e8fc7ef8962a6e3a183b9c42
femto <femtowin@gmail.com>
2012-01-24 04:37:44 +0800

    hide

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @categories = Category.all
+    if params[:set_locale]
+      redirect_to :locale => params[:set_locale]
+    else
+      @categories = Category.all
+    end
   end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
   validates_presence_of :en_name
   validates_presence_of :zh_name
+
+  def name
+    send(I18n.locale.to_s + "_name")
+  end
 end
