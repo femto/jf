ea4e9754fce8a7b8e3d1b4ccb0c43375e76069cc
femto <femtowin@gmail.com>
2012-03-13 15:47:45 +0800

    required

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,16 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
-  validates_presence_of :name,:phone,:company_name,:address,:gongshan,:suiwu,:zuzhijigou
+  validates_presence_of :name, :phone, :company_name, :address, :gongshan, :suiwu, :zuzhijigou
 
-  mount_uploader :product_catalog_image,ImageUploader
-  mount_uploader :business_permit_image,ImageUploader
-  mount_uploader :tax_registration_certificate_image,ImageUploader
-  mount_uploader :organization_code_certificate_image,ImageUploader
+  mount_uploader :product_catalog_image, ImageUploader
+  mount_uploader :business_permit_image, ImageUploader
+  mount_uploader :tax_registration_certificate_image, ImageUploader
+  mount_uploader :organization_code_certificate_image, ImageUploader
+
+
+  validates_presence_of :product_catalog_image
+  validates_presence_of :business_permit_image
+  validates_presence_of :tax_registration_certificate_image
+  validates_presence_of :organization_code_certificate_image
 end
